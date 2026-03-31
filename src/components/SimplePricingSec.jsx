620041d04f69fbd20f1f62e7a3c93971268e9421
ayshaaktersaima1 <ayshaaktersaima1.com>
2026-03-31 21:15:01 +0600

    Updated the simple pricing section design

--- src/components/SimplePricingSec.jsx
+++ src/components/SimplePricingSec.jsx
@@ -34,37 +34,41 @@ const SimplePricingSec = () => {
                     <button className='btn btn-block bg-linear-65 from-[#4f39f6] to-[#9514fa] text-white font-bold rounded-3xl mt-auto'>Get Started Free</button>
                 </div>
                 {/* 2nd card */}
-                <div className='flex flex-col h-full text-left border border-[#f2f2f2] rounded-2xl bg-[#f9fafcFF] p-6'>
-                    <h1 className='text-color text-2xl font-bold pb-2'>Pro</h1>
-                    <p className='text-sec pb-6'>Best for professionals</p>
-                    <h2 className='text-sec text-xl pb-6'><span className='text-color text-[40px] font-bold'>$29</span>/month</h2>
+                <div className='flex flex-col h-full text-left border border-[#f2f2f2] rounded-2xl bg-[#f9fafcFF] p-6 bg-linear-65 from-[#4f39f6] to-[#9514fa] text-white relative'>
+                    <div className='flex justify-center items-center absolute -top-3 left-0 right-0'>
+                        <div className='badge bg-[#fef3c6] 
+                    text-[#bb4d00] rounded-3xl border-none'><span>Most Popular</span></div>
+                    </div>
+                    <h1 className='text-2xl font-bold pb-2'>Pro</h1>
+                    <p className='pb-6'>Best for professionals</p>
+                    <h2 className='text-xl pb-6'><span className='text-[40px] font-bold'>$29</span>/month</h2>
                     <div className='space-y-2 pb-6'>
                         <div className='flex items-center gap-2'>
-                            <Check className='text-[#30b868]' />
-                            <p className='text-sec font-medium'>Access to all premium tools</p>
+                            <Check className='text-white' />
+                            <p className='font-medium'>Access to all premium tools</p>
                         </div>
                         <div className='flex items-center gap-2'>
-                            <Check className='text-[#30b868]' />
-                            <p className='text-sec font-medium'>Unlimited templates</p>
+                            <Check className='text-white' />
+                            <p className='font-medium'>Unlimited templates</p>
                         </div>
                         <div className='flex items-center gap-2'>
-                            <Check className='text-[#30b868]' />
-                            <p className='text-sec font-medium'>Priority support</p>
+                            <Check className='text-white' />
+                            <p className='font-medium'>Priority support</p>
                         </div>
                         <div className='flex items-center gap-2'>
-                            <Check className='text-[#30b868]' />
-                            <p className='text-sec font-medium'>Unlimited projects</p>
+                            <Check className='text-white' />
+                            <p className='font-medium'>Unlimited projects</p>
                         </div>
                         <div className='flex items-center gap-2'>
-                            <Check className='text-[#30b868]' />
-                            <p className='text-sec font-medium'>Cloud sync</p>
+                            <Check className='text-white' />
+                            <p className='font-medium'>Cloud sync</p>
                         </div>
                         <div className='flex items-center gap-2'>
-                            <Check className='text-[#30b868]' />
-                            <p className='text-sec font-medium'>Advanced analytics</p>
+                            <Check className='text-white' />
+                            <p className='font-medium'>Advanced analytics</p>
                         </div>
                     </div>
-                    <button className='btn btn-block bg-linear-65 from-[#4f39f6] to-[#9514fa] text-white font-bold rounded-3xl mt-auto'>Get Started Free</button>
+                    <button className='btn btn-block  font-bold rounded-3xl mt-auto'><span className='bg-linear-65 from-[#4f39f6] to-[#9514fa] bg-clip-text text-transparent'>Start Pro Trial</span></button>
                 </div>
                 {/* 3rd */}
                 <div className='flex flex-col h-full text-left border border-[#f2f2f2] rounded-2xl bg-[#f9fafcFF] p-6'>
